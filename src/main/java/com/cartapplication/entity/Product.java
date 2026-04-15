6a2df2165a8f3ed987a492b382b3707e6f93117b
sindhurmarella1835@gmail.com <sindhurmarella123@gmail.com>
2026-04-15 10:16:38 +0530

    Initial Project Structure

---
+++ src/main/java/com/cartapplication/entity/Product.java
@@ -0,0 +1,8 @@
+package com.cartapplication.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+
+}
---
+++ src/main/java/com/cartapplication/service/InventoryService.java
@@ -0,0 +1,8 @@
+package com.cartapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+
+}
