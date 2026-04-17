b9f76aca34933a181ff55867507703e663a7e158
Shreenithi-C <shreenithii3003@gmail.com>
2026-04-17 10:22:06 +0530

    Added Inventory Repository

--- src/main/java/com/cartapplication/repository/InventoryRepository.java
+++ src/main/java/com/cartapplication/repository/InventoryRepository.java
@@ -1,8 +1,14 @@
 package com.cartapplication.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface InventoryRepository {
+import com.cartapplication.entity.Inventory;
+import com.cartapplication.entity.Product;
 
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProduct(Product product);
 }
