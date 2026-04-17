d51c9444a44eb3a959051a486a028f1e1fb9db41
Shreenithi-C <shreenithii3003@gmail.com>
2026-04-17 12:07:26 +0530

    Added Notification Entity

--- src/main/java/com/cartapplication/entity/Notification.java
+++ src/main/java/com/cartapplication/entity/Notification.java
@@ -1,8 +1,35 @@
 package com.cartapplication.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
-public class Noification {
+@Table(name = "notification")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String message;
+    private String type;
+    
+    // getters and setters
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 }
